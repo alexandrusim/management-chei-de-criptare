2e8f07a73bb0353ee7f6887a0dd881915f1e571a
Vasilca Rares-Mihai <rares-mihai.vasilca@student.tuiasi.ro>
2026-03-21 10:26:24 +0200

    mic update /docs

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+WORKDIR /app/src
+
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- src/main.py
+++ src/main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 from database import engine
 import sql_models
 import routes
+import time
+from sqlalchemy.exc import OperationalError
 
-sql_models.Base.metadata.create_all(bind=engine)
+MAX_RETRIES = 5
+RETRY_DELAY = 5  # secunde
+
+for attempt in range(MAX_RETRIES):
+    try:
+        sql_models.Base.metadata.create_all(bind=engine)
+        break
+    except OperationalError as e:
+        if attempt == MAX_RETRIES - 1:
+            raise e
+        time.sleep(RETRY_DELAY)
 
 app = FastAPI(title="Sistem management chei - Proiect SI")
 
